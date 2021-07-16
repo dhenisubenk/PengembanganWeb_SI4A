f16cdf25bb3013d803c5996b084fb433972a8f40
dhenisubenk <38071294+dhenisubenk@users.noreply.github.com>
2021-07-16 16:00:31 +0900

    week9

---
+++ week9.php
@@ -0,0 +1,80 @@
+<?php
+    //variabel
+    echo $_GET['nama'];
+    echo "<br>";
+    echo $_GET['nilai'];
+    echo "<br>";
+
+    // $nama = "Kamarudin";
+    // $nama .= " Jaenudin";
+    // $nilai = 90;
+
+    //get form
+    // var_dump($_POST);
+    // $nama = $_GET['nama'];
+    // $nim = $_GET['nim'];
+    // $jurusan = $_GET['jurusan'];
+    // $alamat = $_GET['alamat'];
+
+    // echo "Halo $nama, Nim Anda $nim, Jurusan $jurusan, Alamat $alamat";
+    // $nama = $nama + $nama
+
+    //alert('Halo ' + nama);
+    //echo "Halo $nama";
+
+    $nilai = 90;
+    $grade = $status = "";
+    //percabangan
+    if($nilai >= 80 && $nilai <= 100){
+        $grade = "A";
+    }else if($nilai >= 70){
+        $grade = "B";
+    }else if($nilai >= 60){
+        $grade = "C";
+    }else if($nilai >= 50){
+        $grade = "D";
+    }else{
+        $grade = "E";
+    }
+
+    switch ($grade) {
+        case 'A':
+        case 'B':
+        case 'C':
+            $status = "Lulus";
+            break;
+        case 'D':
+            $status = "Perbaikan";
+            break;
+        case 'E':
+            $status = "Tidak Lulus";
+            break;
+        default:
+            # code...
+            break;
+    }
+    
+    //perulangan
+    for($i = 1; $i <= 5; $i++)
+    {
+        echo "For $i<br>";
+    }
+
+    //while
+    $j = 1;
+    while($j <= 7)
+    {
+        echo "While $j <br>";
+        $j++;
+    }
+
+    //do while
+    $k = 1;
+    do {
+        echo "Do While $k <br>";
+        $k++;
+    } while ($k <= 10);
+
+
+    echo "$grade : $status";
+?>
